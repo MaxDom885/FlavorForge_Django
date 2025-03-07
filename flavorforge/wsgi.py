02c10c972523cb466d72b66e5ff5115273b17606
Masmoud <maxdomyacoubou@gmail.com>
2025-03-07 20:34:46 +0100

    Modif de wsgi

--- flavorforge/wsgi.py
+++ flavorforge/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for flavorforge project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
-from django.core.wsgi import get_wsgi_application
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flavorforge.settings')
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
